dc61616d655848d69d2e229b845e02aac571e3cd
Lucas Schwartz dos Santos <schwartzlucas.santos@gmail.com>
2022-11-19 15:55:02 -0300

    nodo_e_construtor

--- rubroNegra.java
+++ rubroNegra.java
@@ -1,4 +1,31 @@
 public class rubroNegra {
-    private Integer element;
-    uuabsdfcbasbef
+    private static final class Node {
+        public Node father;
+        public Node left;
+        public Node right;
+        public Integer element;
+        public char cor;
+ 
+        public Node(Integer element) {
+            father = null;
+            left = null;
+            right = null;
+            this.element = element;
+            cor = 'v';
+        }
+    }
+
+    // Atributos da árvore
+    private int count;  // número de elementos
+    private Node root;  // referencia para a raiz
+    private Node nil;  // nodo sentinela
+
+    public rubroNegra() {
+        count = 0;
+        root = null;
+        nil.element = null;
+        nil.cor = 'p';
+    }
+
+
 }
